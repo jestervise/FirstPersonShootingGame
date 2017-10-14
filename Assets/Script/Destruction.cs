1fac74351d2dc632db36a4b7540735625d867922
mj <chang_mingjun@live.com>
2017-10-14 22:30:50 +0800

    imported assets,placed assets and basic control

---
+++ Assets/Script/Destruction.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destruction : MonoBehaviour {
+    public GameObject crackedVersion;
+    private void OnMouseDown()
+    {
+        Instantiate(crackedVersion, transform.position, Quaternion.identity);
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Script/Gun.cs
@@ -0,0 +1,30 @@
+﻿
+using UnityEngine;
+
+public class Gun : MonoBehaviour {
+    public float distance=100f;
+    public float damage = 10f;
+    public Camera fpsCam;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+      
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+	}
+
+    void Shoot() {
+        RaycastHit hit;
+        if (Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, distance))
+        {
+            Debug.Log(hit.transform.name);
+        }
+    }
+}
